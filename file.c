1f48f46d387978e4d0a08408e287d1e3adb71bbf
Rui Ueyama <rui314@gmail.com>
2010-06-21 21:52:13 -0700

    added file.c

---
+++ file.c
@@ -0,0 +1,54 @@
+#include "8cc.h"
+
+void error(char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    fprintf(stderr, "8cc: ");
+    vfprintf(stderr, format, ap);
+    fprintf(stderr, "\n");
+    exit(-1);
+}
+
+File *make_file(FILE *stream, char *filename) {
+    File *r = malloc(sizeof(File));
+    r->stream = stream;
+    r->line = 1;
+    r->filename = make_string();
+    ostr(r->filename, filename);
+    r->ungotten = EOF;
+    return r;
+}
+
+void unreadc(int c, File *file) {
+    if (file->ungotten != EOF)
+	ungetc(file->ungotten, file->stream);
+    file->ungotten = c;
+}
+
+int readc(File *file) {
+    int c;
+    if (file->ungotten == EOF) {
+	c = getc(file->stream);
+    } else {
+	c = file->ungotten;
+	file->ungotten = EOF;
+    }
+    if (c == EOF || c == '\0') return EOF;
+    if (c == '\r') {
+	file->line++;
+	c = getc(file->stream);
+	if (c == '\n') return c;
+	unreadc(c, file);
+	return '\n';
+    }
+    if (c == '\n') file->line++;
+    return c;
+}
+
+int getfileline(File *file) {
+    return file->line;
+}
+
+char *getfilename(File *file) {
+    return STRING_BODY(file->filename);
+}
--- tests.c
+++ tests.c
@@ -1,7 +1,21 @@
 #include "8cc.h"
 
-#define NOT_NULL(p) do { if (!(p)) error("Line %d: must not be null " #p, __LINE__); } while (0)
-#define EQ(x, y) do { if ((x) != (y)) error("Line %d: must be the same", __LINE__); } while (0)
+#define NOT_NULL(p) do { if (!(p)) error("line %d: must not be null " #p, __LINE__); } while (0)
+#define EQ(x, y) do { if ((x) != (y)) error("line %d: must be the same", __LINE__); } while (0)
+#define EQ_CHAR(x, y) do { eq_char(__LINE__, (x), (y)); } while (0)
+#define EQ_STR(x, y)  do { eq_str(__LINE__, (x), (y)); } while (0)
+
+void eq_str(int line, char *expected, char *got) {
+    if (strcmp(expected, got)) {
+	error("line %d: \"%s\" expected, but got \"%s\"", line, expected, got);
+    }
+}
+
+void eq_char(int line, int expected, int got) {
+    if (expected != got) {
+	error("line %d: '%d' expected, but got '%d'", line, expected, got);
+    }
+}
 
 /*
  * String
@@ -14,7 +28,7 @@ int64_t qword(char *str) {
     return r;
 }
 
-void testString(void) {
+void test_string(void) {
     String *b = make_string();
     NOT_NULL(b);
     EQ(STRING_LEN(b), 0);
@@ -28,8 +42,45 @@ void testString(void) {
     EQ(strcmp(STRING_BODY(b), "abcdefghi"), 0);
 }
 
+static FILE *create_file(char *content) {
+    char tmpl[] = "tmpXXXXXX";
+    int fd = mkstemp(tmpl);
+    if (fd < 0) {
+	perror("fd: ");
+	exit(-1);
+    }
+    unlink(tmpl);
+    write(fd, content, strlen(content));
+    lseek(fd, 0, SEEK_SET);
+    return fdopen(fd, "r");
+}
+
+void test_file(void) {
+    char *data = "ab\nc\r\r\nd\r";
+
+    FILE *stream = create_file(data);
+    File *file = make_file(stream, "pipe");
+    EQ_STR(getfilename(file), "pipe");
+
+    EQ(1, getfileline(file));
+    EQ_CHAR('a', readc(file));
+    EQ_CHAR('b', readc(file));
+    EQ_CHAR('\n', readc(file));
+    EQ(2, getfileline(file));
+    EQ_CHAR('c', readc(file));
+    EQ_CHAR('\n', readc(file));
+    EQ(3, getfileline(file));
+    EQ_CHAR('\n', readc(file));
+    EQ(4, getfileline(file));
+    EQ_CHAR('d', readc(file));
+    EQ_CHAR('\n', readc(file));
+    EQ(5, getfileline(file));
+    EQ_CHAR(EOF, readc(file));
+}
+
 int main(int argc, char **argv) {
-    testString();
+    test_string();
+    test_file();
     printf("OK\n");
     return 0;
 }
