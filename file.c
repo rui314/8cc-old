e198938805bef956e4870a9988a22bfa837194f0
Rui Ueyama <rui314@gmail.com>
2010-06-27 10:38:53 -0700

    Created error.c and elf.c.

--- file.c
+++ file.c
@@ -1,22 +1,5 @@
 #include "8cc.h"
 
-void error(char *format, ...) {
-    va_list ap;
-    va_start(ap, format);
-    fprintf(stderr, "8cc: ERROR: ");
-    vfprintf(stderr, format, ap);
-    fprintf(stderr, "\n");
-    exit(-1);
-}
-
-void warn(char *format, ...) {
-    va_list ap;
-    va_start(ap, format);
-    fprintf(stderr, "8cc: ");
-    vfprintf(stderr, format, ap);
-    fprintf(stderr, "\n");
-}
-
 File *make_file(FILE *stream, char *filename) {
     File *r = malloc(sizeof(File));
     r->stream = stream;
