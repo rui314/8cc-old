8fdc9a6edbcda78071c5d0ad804d68b0b41720c7
Rui Ueyama <rui314@gmail.com>
2010-08-19 16:56:35 -0700

    Added decl.c.

---
+++ decl.c
@@ -0,0 +1,8 @@
+/*
+ * decl.c - C declaration parser
+ *
+ * Copyright 2010 Rui Ueyama <rui314@gmail.com>.  All rights reserved.
+ * This code is available under the simplified BSD license.  See LICENSE for details.
+ */
+
+#include "8cc.h"
